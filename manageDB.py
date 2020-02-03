d46a2545a70f8d00e298e6a4f2aaed45187859ff
Ryge76 <ggmaster@msn.com>
2020-02-03 14:43:34 +0100

    Installed mysql-connector in Pipenv. Created manageDB.py

---
+++ manageDB.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+"""
+Etpaes:
+1. initialiser la connection avec la DB
+2. Remplir la BD
+3. Faire des requêtes select pour chercher un produit
+3. Faire des requêtes select pour trouver un équivalent
+4. Enregistrer le substitut dans la BD
+
+5. 
+"""
+
+
